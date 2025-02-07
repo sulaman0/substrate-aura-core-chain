2d37354f69e516f2a58948f180bbd0f3c6d8f9e9
sulaman0 <symikhan70@gmail.com>
2025-02-07 17:04:41 +0500

    Add benchmarking.rs cargo for loading
    Signed-off-by: sulaman0 <symikhan70@gmail.com>

---
+++ template/node/src/benchmarking.rs
@@ -0,0 +1,17 @@
+//! Contains code to setup the command invocations in [`super::command`] which would
+//! otherwise bloat that module.
+
+use std::{sync::Arc, time::Duration};
+
+use scale_codec::Encode;
+// Substrate
+use sc_cli::Result;
+use sc_client_api::BlockBackend;
+use sp_core::{ecdsa, Pair};
+use sp_inherents::{InherentData, InherentDataProvider};
+use sp_runtime::{generic::Era, OpaqueExtrinsic, SaturatedConversion};
+// Frontier
+use fp_account::AccountId20;
+use frontier_template_runtime::{self as runtime, AccountId, Balance, BalancesCall, SystemCall};
+
+use crate::service::Client;
