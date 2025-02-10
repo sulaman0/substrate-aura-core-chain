baee4a21899491ef01ccd5e5016451d6dcc326f8
sulaman0 <symikhan70@gmail.com>
2025-02-10 11:24:56 +0500

    Cli for sub commands.
    Signed-off-by: sulaman0 <symikhan70@gmail.com>

--- template/node/src/cli.rs
+++ template/node/src/cli.rs
@@ -28,4 +28,42 @@ pub struct Cli {
 }
 
 
+#[derive(Debug, clap::Subcommand)]
+pub enum Subcommand {
+	/// Key management cli utilities
+	#[command(subcommand)]
+	Key(sc_cli::KeySubcommand),
 
+	/// Build a chain specification.
+	BuildSpec(sc_cli::BuildSpecCmd),
+
+	/// Validate blocks.
+	CheckBlock(sc_cli::CheckBlockCmd),
+
+	/// Export blocks.
+	ExportBlocks(sc_cli::ExportBlocksCmd),
+
+	/// Export the state of a given block into a chain spec.
+	ExportState(sc_cli::ExportStateCmd),
+
+	/// Import blocks.
+	ImportBlocks(sc_cli::ImportBlocksCmd),
+
+	/// Remove the whole chain.
+	PurgeChain(sc_cli::PurgeChainCmd),
+
+	/// Revert the chain to a previous state.
+	Revert(sc_cli::RevertCmd),
+
+	/// Sub-commands concerned with benchmarking.
+	#[cfg(feature = "runtime-benchmarks")]
+	#[command(subcommand)]
+	Benchmark(frame_benchmarking_cli::BenchmarkCmd),
+
+	/// Sub-commands concerned with benchmarking.
+	#[cfg(not(feature = "runtime-benchmarks"))]
+	Benchmark,
+
+	/// Db meta columns information.
+	FrontierDb(fc_cli::FrontierDbCmd),
+}
