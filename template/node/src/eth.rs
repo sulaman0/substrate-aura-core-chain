4177dd43a2d9ed74c707a7bb9a7ec505fab7ca54
sulaman0 <symikhan70@gmail.com>
2025-02-18 18:34:23 +0500

    // Frontier
    Signed-off-by: sulaman0 <symikhan70@gmail.com>

--- template/node/src/eth.rs
+++ template/node/src/eth.rs
@@ -12,3 +12,6 @@ use sc_executor::HostFunctions;
 use sc_network_sync::SyncingService;
 use sc_service::{error::Error as ServiceError, Configuration, TaskManager};
 use sp_api::ConstructRuntimeApi;
+use sp_core::H256;
+use sp_runtime::traits::Block as BlockT;
+// Frontier
